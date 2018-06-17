bff67ef24f0008a67ad05a617a734594edb17f62
Leensey Lawas <lmlawas@up.edu.ph>
2018-06-18 04:34:08 +0800

    Add data import guide

---
+++ data/actual flow data/README.md
@@ -0,0 +1,1 @@
+_thesis\_data.sql_ contains the actual data that the researcher used in the program. See _import\_data.sh_ to import _thesis_data.sql_ to your database.
---
+++ data/actual flow data/import_data.sh
@@ -0,0 +1,5 @@
+# Import thesis_data.sql and local_ip_addr.sql to your machine's database
+# Replace <username> and <database_name> with appropriate values
+mysql -u <username> -p <database_name> < thesis_data.sql
+mysql -u <username> -p <database_name> < local_ip_addr.sql
+
